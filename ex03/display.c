5a5d7b4a6b0ee04bbb28ad648d5c5c9a12a3187f
juliandekievid <juliandekievid@gmail.com>
2020-07-07 23:55:34 +0200

    new file:   Makefile 	new file:   Makefile~ 	modified:   display.c 	new file:   display.o 	new file:   displayFunctions.o 	new file:   syntaxCheck.c 	new file:   syntaxCheck.o

--- ex03/display.c
+++ ex03/display.c
@@ -9,16 +9,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "displayFunctions.h"
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+
 
 void parentProces(void);
 void childOneProces(void);
 void childTwoProces(void);
 
+char printMethod, printChar, printFirstChar,printSecondChar, printThirdChar;
+unsigned long int numOfTimes;
+unsigned int niceIncr;
+ErrCode err;
+
 int main(int argc, char *argv[]) {
-  char printMethod, printFirstChar,printSecondChar, printThirdChar;
-  unsigned long int numOfTimes;
-  unsigned int niceIncr;
-  ErrCode err;
   
   err = SyntaxCheck(argc, argv);  // Check the command-line parameters
   if(err != NO_ERR) {
@@ -30,20 +36,50 @@ int main(int argc, char *argv[]) {
     printFirstChar = argv[4][0];
     printSecondChar = argv[5][0];
     printThirdChar = argv[6][0];
-    
-    PrintCharacters(printMethod, numOfTimes, printChar);  // Print character printChar numOfTimes times using method printMethod
   }
-  
   printf("\n");  // Newline at end
 
+  pid_t pid = fork();
+  switch(pid)
+  {
+  case -1: printf("Error, failed to fork"); 
+     break;
+  case 0: childOneProces(); 
+     break;
+  default: parentProces();
+  }
+
+return 0;
+}
+
+void childOneProces(void){
+
+
+   nice(0);
+   printChar = printFirstChar;
+   PrintCharacters(printMethod, numOfTimes, printChar);  // Print character printChar numOfTimes times using method printMethod
+  }
+
+void childTwoProces(void){
+   int incr = niceIncr;
+   nice(incr);
+   printChar = printSecondChar;
+   PrintCharacters(printMethod, numOfTimes, printChar);  // Print character printChar numOfTimes times using method printMethod
+  }
+
+void parentProcess(void){
    pid_t pid = fork();
-   switch(pid)
-   {
-   case -1: printf("Error, failed to fork"); 
-      break;
-   case 0: childOneProces(); 
-      break;
-   default: parentProces();
+   if (pid < 0){
+   printf("rror, failed to fork");}
+   else if (pid == 0) {childTwoProces();}
+
+   else { 
+   int incr = 2*niceIncr;
+   nice(incr);
+   printChar = printThirdChar;
+   PrintCharacters(printMethod, numOfTimes, printChar);  // Print character printChar numOfTimes times using method     printMethod
+   wait(NULL);
+   wait(NULL);
+   //printf(
    }
-  return 0;
 }
