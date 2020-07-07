3027475d473d828d19f9a40f882c9b4a677a43db
juliandekievid <juliandekievid@gmail.com>
2020-07-07 22:00:29 +0200

    Init ex02

---
+++ ex02/syntaxCheck.c
@@ -0,0 +1,40 @@
+/******************************************************************************
+ * File:         syntaxCheck.c
+ * Version:      1.4
+ * Datum:        2018-02-20
+ * Author:       M. van der Sluys, J.H.L. Onokiewicz, R.B.A. Elsinghorst, J.G. Rouland
+ * Description:  OPS exercise 2:  definitions of test functions for display.c
+ ******************************************************************************/
+
+#include <stdlib.h>
+#include "syntaxCheck.h"
+
+
+// Test whether an argument is one character long and has the correct value (e,p,w):
+ErrCode TestType(char *printMethod) {
+  ErrCode fout = NO_ERR;
+  
+  // Add the missing code
+  
+  return fout;
+}
+
+
+// Test whether an argument contains a non-negative number:
+ErrCode TestNr(char *numberOfTimes) {
+  ErrCode fout = NO_ERR;
+  
+  // Add the missing code
+  
+  return fout;
+}
+
+
+// Test whether an argument contains only one character:
+ErrCode TestChar(char *printChar) {
+  ErrCode fout = NO_ERR;
+  
+  // Add the missing code
+  
+  return fout;
+}
