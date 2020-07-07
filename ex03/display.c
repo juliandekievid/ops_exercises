7be9fbc1b8a5abd42be78f08a8e8ba97bddae3dd
juliandekievid <juliandekievid@gmail.com>
2020-07-07 22:59:20 +0200

    modified:   display.c

--- ex03/display.c
+++ ex03/display.c
@@ -10,9 +10,14 @@
 #include <stdlib.h>
 #include "displayFunctions.h"
 
+void parentProces(void);
+void childOneProces(void);
+void childTwoProces(void);
+
 int main(int argc, char *argv[]) {
+  char printMethod, printFirstChar,printSecondChar, printThirdChar;
   unsigned long int numOfTimes;
-  char printMethod, printChar;
+  unsigned int niceIncr;
   ErrCode err;
   
   err = SyntaxCheck(argc, argv);  // Check the command-line parameters
@@ -21,11 +26,24 @@ int main(int argc, char *argv[]) {
   } else {
     printMethod = argv[1][0];
     numOfTimes = strtoul(argv[2], NULL, 10);  // String to unsigned long
-    printChar = argv[3][0];
+    niceIncr = argv[3][0];
+    printFirstChar = argv[4][0];
+    printSecondChar = argv[5][0];
+    printThirdChar = argv[6][0];
     
     PrintCharacters(printMethod, numOfTimes, printChar);  // Print character printChar numOfTimes times using method printMethod
   }
   
   printf("\n");  // Newline at end
+
+   pid_t pid = fork();
+   switch(pid)
+   {
+   case -1: printf("Error, failed to fork"); 
+      break;
+   case 0: childOneProces(); 
+      break;
+   default: parentProces();
+   }
   return 0;
 }
