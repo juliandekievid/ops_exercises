d2d616be89cf78b75c30cfa4780a1e72ce666dc7
juliandekievid <juliandekievid@gmail.com>
2020-07-20 22:09:03 +0200

    modified:   buildnumber.num 	new file:   queue.txt 	new file:   sharedQueue 	modified:   sharedQueue.c 	modified:   sharedQueue.o

--- ex08/sharedQueue.c
+++ ex08/sharedQueue.c
@@ -7,11 +7,12 @@
  ******************************************************************************/
 
 #include "Queue.h"
-#include <studio.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <pthread.h>
 #include <signal.h>
+#include <string.h>
 
 data_t thread_PA = {2, "Thread producer A"}; // writes data to queue every 2 seconds
 data_t thread_PB = {3, "Thread producer B"}; // writes data to queue every 3 seconds
@@ -41,33 +42,34 @@ int main()
 
    // Define SHR
    memset(&act, '\0', sizeof(act));
-   act.sa_handler = newHandler1;
+   act.sa_handler = sigHandler;
    act.sa_flags = 0;
    sigemptyset(&act.sa_mask);
    // Install SHR
    sigaction(SIGINT, &act, &oldact);
 
-   pthread_create(&thread_PA_ID, NULL, &producerThread, (void *)&thread_A));
+   pthread_create(&thread_PA_ID, NULL, &producerThread, (void *)&thread_PA);
    pthread_join(thread_PA_ID, NULL);
 
-   pthread_create(&thread_PB_ID, NULL, &producerThread, (void *)&thread_B));
+   pthread_create(&thread_PB_ID, NULL, &producerThread, (void *)&thread_PB);
    pthread_join(thread_PB_ID, NULL);
 
-   pthread_create(&thread_PC_ID, NULL, &producerThread, (void *)&thread_C));
+   pthread_create(&thread_PC_ID, NULL, &producerThread, (void *)&thread_PC);
    pthread_join(thread_PC_ID, NULL);
 
-   pthread_create(&thread_CA_ID, NULL, &consumerThread, NULL;  
+   pthread_create(&thread_CA_ID, NULL, &consumerThread, NULL);  
    pthread_join(thread_CA_ID, NULL);
-
+   
 }
 
 void *producerThread(void *arg){
-   data_t *data = arg;
+   //data_t *data = arg;
    while(1)
       {
+         data_t *arguments = arg;
          sleep(arguments->intVal);
          pthread_mutex_lock(&lock);
-         data_t *arguments = arg;
+         
       
       if(backQueue(&queue) == NULL){
          createQueue(&queue, *arguments);      
@@ -75,7 +77,7 @@ void *producerThread(void *arg){
          pushQueue(&queue, *arguments);      
       }
          pthread_mutex_unlock(&lock);      
-      if(crtl_C_pressed == 1){
+      if(ctrl_C_pressed == 1){
          activeThreads--;
          pthread_exit(NULL);      
       }
@@ -84,16 +86,39 @@ void *producerThread(void *arg){
 
 void *consumerThread(void *arg){
    while(1){
-      sleep(15);   
-      pthread_mutex_lock(&lock);
-      showQueue(&queue);
-      FP = fopen("textfile.txt", "w");
-      const node
-      
+   if(arg == NULL){;}
+   sleep(15);   
+   pthread_mutex_lock(&lock);
+   FP = fopen("queue.txt", "w");
+ 
+   showQueue(&queue);
+   const node_t* nextN = queue.lastNode;
+
+   if(nextN == NULL){
+      fprintf(FP, "Queue is empty, last node is %p\n", (void*) queue.lastNode);
+   }else{
+      fprintf(FP, "Queue contains %ld nodes:\n", sizeQueue(&queue));
+      fprintf(FP, "Last node: %p\n", (void*) queue.lastNode);
+      do {
+         nextN = nextN->nextNode;
+         fprintf(FP, "pNode = %p  Data = '%d' '%s'  nextN = %p\n", 
+                (void*)nextN, nextN->data.intVal, nextN->data.text, (void*)nextN->nextNode);      
+      }while(nextN != queue.lastNode);      
    }
+   fclose(FP);
+   deleteQueue(&queue);
+   pthread_mutex_unlock(&lock);
+
+   if(activeThreads <= 0){
+      pthread_exit(NULL);
+   }      
+} 
+  return 0;
+
+
 }
 
 void sigHandler(int signal){
    ctrl_C_pressed = 1;
-   printf("Program terminated by Crtl-C");
+   printf("Program terminated by Crtl-C\n");
 }
