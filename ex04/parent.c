d9bed397f0e8a007cc48e4b05bcf185b21eb00be
juliandekievid <juliandekievid@gmail.com>
2020-09-23 17:16:42 +0200

    Working exercise

--- ex04/parent.c
+++ ex04/parent.c
@@ -13,9 +13,6 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 
-//char printMethod, printChar, printFirstChar,printSecondChar, printThirdChar;
-//unsigned long int numOfTimes;
-
 ErrCode err = NO_ERR;
 
 int main(int argc, char *argv[]) {
@@ -23,56 +20,56 @@ int main(int argc, char *argv[]) {
   err = SyntaxCheck(argc, argv);  // Check the command-line parameters
   if(err != NO_ERR) {
     DisplayError(err);        // Print an error message  
-  
   } else {
-   // numOfTimes = strtoul(argv[2], NULL, 10);  // String to unsigned long
-  niceValue = strtoul(argv[3], NULL, 10);
-  printf("\n");  // Newline at end
-
-  switch(fork())  //Parent and child 1 carry out switch()
-  {
-  case -1: 
-      printf("Error, failed to fork(1)"); 
-      break;
-  case 0: //Child one
-      nice(niceValue);
-      execl("../ex02/display", "display", argv[1], argv[2], argv[4], (char *) NULL);
-      printf("Starting child 1 failed\n");
-      exit(1);
-      break;
-  default:  //Parent
-      switch(fork()){ //create child 2
-        case -1:
-          printf("Error, failed to fork(2)"); 
-          break;
-        case 0:
-          nice(2*niceValue);
-          execl("../ex02/display", "display",  argv[1], argv[2], argv[5], (char *) NULL);
-          printf("Starting child 2 failed\n");
-          exit(1);
-          break;
-        default:
-          switch(fork()){ // Create child 3
-            case -1: 
-              printf("Error, failed to fork(3)"); 
-              break;
-            case 0: // Child 3
-              nice(0);
-              execl("../ex02/display", "display",  argv[1], argv[2], argv[6], (char *) NULL);
-              printf("Starting child 3 failed\n");
-              exit(1);
-              break;
-            default:
-              wait(NULL);
-              wait(NULL);
-              printf("All children have finished\n");
-              break;
-          }   
-          break;
-   }
-      break;
+    int niceValue = strtoul(argv[3], NULL, 10);
+    switch(fork())  //Parent and child 1 carry out switch()
+      {
+      case -1: 
+	printf("Error, failed to fork(1)"); 
+	break;
+      case 0: //Child one
+	nice(niceValue);
+	execl("../ex02/display", "display", argv[1], argv[2], argv[4], (char *) NULL);
+	printf("Starting child 1 failed\n");
+	exit(1);
+	break;
+      default:  //Parent
+	switch(fork())
+	  { //create child 2
+	  case -1:
+	    printf("Error, failed to fork(2)"); 
+	    break;
+	  case 0:
+	    nice(2*niceValue);
+	    execl("../ex02/display", "display",  argv[1], argv[2], argv[5], (char *) NULL);
+	    printf("Starting child 2 failed\n");
+	    exit(1);
+	    break;
+	  default:
+	    switch(fork()){ // Create child 3
+	    case -1: 
+	      printf("Error, failed to fork(3)"); 
+	      break;
+	    case 0: // Child 3
+	      nice(0);
+	      execl("../ex02/display", "display",  argv[1], argv[2], argv[6], (char *) NULL);
+	      printf("Starting child 3 failed\n");
+	      exit(1);
+	      break;
+	    default:
+	      break;
+	    }  
+	  }
+	
+	wait(NULL);
+	wait(NULL);
+	wait(NULL);
+	printf("All children have finished\n");
+	printf("\n");  // Newline at end
+	break;	 
+      }
   }
-
-return 0;
+  
+  return 0;
 }
 
